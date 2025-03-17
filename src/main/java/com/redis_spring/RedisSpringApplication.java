a1ae846b946e74e744ff431cb21d7f1068df3fea
geetoor-maven <agus40994@gmail.com>
2025-03-17 22:22:19 +0700

    Feat : Spring Boot with Cache

--- src/main/java/com/redis_spring/RedisSpringApplication.java
+++ src/main/java/com/redis_spring/RedisSpringApplication.java
@@ -2,8 +2,10 @@ package com.redis_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class RedisSpringApplication {
 
 	public static void main(String[] args) {
